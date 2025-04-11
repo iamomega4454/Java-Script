d042baa1642b1a1c8db2186f82f50fedc7b54ddc
AMAL KURIAN ROYCE <145897989+iamomega4454@users.noreply.github.com>
2025-04-11 15:06:01 +0000

    Added JavaScript file

---
+++ basis/Constants_Varibles.js
@@ -0,0 +1,14 @@
+const accountNumber = 035798634
+let emailId = "iamomega1403@gmail.com"
+var password = "123456789"                  //do not use var ->issue in block scope and functional scope                                                   
+accountType = "NRI"
+let accountState;//undefined
+
+// accountId = 2  constants
+//  cannot be initialised twice
+
+password = "var32647"
+console.log(accountNumber);
+
+console.table([accountNumber,emailId,password,accountType,accountState])
+
