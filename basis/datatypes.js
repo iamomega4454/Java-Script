7e23ca9456eef1e2e531cd0f4b65608afca5cf3d
AMAL KURIAN ROYCE <145897989+iamomega4454@users.noreply.github.com>
2025-04-11 15:45:27 +0000

    done

---
+++ basis/datatypes.js
@@ -0,0 +1,12 @@
+let name = "amal"//string
+let age = 25 //number  also use bigint -> for big numbers
+let type = true //boolean
+let workingstate = null  //standalone value or empty
+
+console.log(typeof name)  //string
+console.log(typeof age)  //number
+console.log(typeof type)    //boolean
+console.log(typeof workingstate)   //object
+
+
+
