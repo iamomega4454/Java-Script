ad113a4655097942e97238adeeb058327ac988a7
AMAL KURIAN ROYCE <145897989+iamomega4454@users.noreply.github.com>
2025-04-12 08:23:51 +0000

    datatypeConversion.js

--- basis/datatypeConversion.js
+++ basis/datatypeConversion.js
@@ -18,3 +18,43 @@ let val2 = String(num2)
 console.log(typeof val2)
 console.log(val2)   //value will be 44 but it would be string 
 
+//**********************************OPERATIONS**************************************
+
+let value = 3
+let negval = -value
+console.log(negval);
+
+console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2%2);
+console.log(2/2);
+
+let str1 = "hello"
+let str2 = " boy"
+let str3 = str1+str2
+console.log(str3);  //combine 2 words
+
+console.log(2 + 3); //5
+console.log("2" + 3);  //23 
+console.log("2" + "3"); //23
+console.log("2" + 4 + 3);   //243
+console.log(2 + 4 + "3");   //63
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
