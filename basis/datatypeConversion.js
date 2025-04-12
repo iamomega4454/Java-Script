598421ef883fcb147431d29d709fee2382e5f897
AMAL KURIAN ROYCE <145897989+iamomega4454@users.noreply.github.com>
2025-04-12 08:56:25 +0000

    datatypeConversion.js

--- basis/datatypeConversion.js
+++ basis/datatypeConversion.js
@@ -38,8 +38,24 @@ console.log(str3);  //combine 2 words
 console.log(2 + 3); //5
 console.log("2" + 3);  //23 
 console.log("2" + "3"); //23
-console.log("2" + 4 + 3);   //243
+console.log("2" + 4 + 3);   //243             
 console.log(2 + 4 + "3");   //63
+
+console.log(+true); //1
+let gameCounter1 = 103
+let a = gameCounter1++              //here value of a is 103 then it increment it
+console.log(gameCounter1);  //104
+console.log(a); //103
+
+
+let gameCounter2 = 103
+let b = ++gameCounter2              //here the value of b is incremented 103 
+console.log(gameCounter2);  //104
+console.log(b); //104
+
+
+
+
 
 
 
