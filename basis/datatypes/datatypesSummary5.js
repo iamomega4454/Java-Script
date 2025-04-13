91e530ff38062ae62c49feca5437f7589bcf0869
AMAL KURIAN ROYCE <145897989+iamomega4454@users.noreply.github.com>
2025-04-13 13:19:16 +0000

    datatypesSummary5.js

--- basis/datatypes/datatypesSummary5.js
+++ basis/datatypes/datatypesSummary5.js
@@ -1,18 +1,29 @@
 // primitive -> 7types
 // String , Number, Boolean, null, BigInt,undefined, Symbol
-
 const score = 135
 const scoreval = 167.45    // Number
 const isLoggedin = false   //Boolean 
-const temp = null
+const temp = null   //object
 let useremail; //undefined 
 let BigNum =76545678987654567n //BigInt
 const id = Symbol('147')           // both are having different value
-const anotherid = Symbol('147')
+const anotherid = Symbol('147') 
 console.log(id == anotherid);   //    checking -> false
-
-
-
+//**************************************************************************** */
 
 // nonprinitive  (reference)->3types
 // Array, Objects, Functions
+
+const heros = ["Batman","Superman","Wonderwomen","Flash","Aquaman"]     //array
+let myDetails = {
+    name : "Amal",
+    age : 22 
+};
+console.log(myDetails); //object
+
+const MyFunction = function(){
+    console.log("Hello world");
+};
+console.log( typeof MyFunction); //function
+
+//**************************************************************************** */
