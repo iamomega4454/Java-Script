f90ec93d94ba4439435d91d87de7d4321fefe358
AMAL KURIAN ROYCE <145897989+iamomega4454@users.noreply.github.com>
2025-04-13 13:59:01 +0000

    datatypesSummary5.js

--- basis/datatypes/datatypesSummary5.js
+++ basis/datatypes/datatypesSummary5.js
@@ -34,3 +34,28 @@ console.log( typeof MyFunction); //function
 // 2 types of memmory -> stack and heap
 // stack -> primitive data types
 // heap -> non primitive data types
+
+// stack*******************8
+let a = 5;
+let b = a;  //copying the value of a to b                   
+console.log(a); //5
+console.log(b); //5
+b = 10; //changing the value of b
+console.log(a); //5
+console.log(b); //10
+
+
+// heap*******************8
+let gpayDetails = {
+    name : "Amal",
+    upi : "Amal243@okhdfc"
+};
+let gpayDetails1 = gpayDetails.name; //copying the reference of gpayDetails to gpayDetails1
+console.log(gpayDetails); //object printing
+console.log(gpayDetails1); //object printing
+gpayDetails1.name = "dominic";
+console.log(gpayDetails); //object printing
+console.log(gpayDetails1); //object printing);
+// here the the value of gpayDetails1 is changed but the value of gpayDetails is not changed
+// this is because the value of gpayDetails1 is copied to gpayDetails and not the reference
+// so the value of gpayDetails is not changed
