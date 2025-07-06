f956264076c3dfbea90b94917cd18519b1f5916c
AMAL KURIAN ROYCE <145897989+iamomega4454@users.noreply.github.com>
2025-07-06 14:51:14 +0000

    ControlFlow12.js

--- basis/ControlFlow12.js
+++ basis/ControlFlow12.js
@@ -30,4 +30,62 @@ if (temperature===40) {
 }
 
 
+//implicit scope (for single line ie.without "{}")but can be written multiple lines by using " , "
+
+// use of else if
+const bal=30
+if (bal<20) {
+    console.log("less than 20");
+}else if (bal===20) {
+    console.log("equal to 20");
+}else{
+    console.log("greater than 20");
+}
+
+
+const userloggin = true
+const debitcard = true
+const loggedinfromgoogle=false
+const loggedinfromemali=true
+
+if (userloggin && debitcard) {
+    console.log('allow to buy course ');
+    if (loggedinfromgoogle || loggedinfromemali) {
+        console.log('welcome employee');
+    }else{
+        console.log('welcome guest user');
+    }
+}
+
+// switch case
+const month=3
+switch (month) {
+    case 1:                     // if string then    case "april"*********************
+        console.log("January");
+        break;
+    case 2:
+        console.log("February");
+        break;
+    case 3:
+        console.log("march");
+        break;
+    case 4:
+        console.log("april");
+        break;
+    case 5:
+        console.log("may");
+        break;
+    case 8:
+        console.log("june");
+        break;
+
+    default:
+        console.log('invalid month');
+        break;
+}
+
+
+
+
+
 
