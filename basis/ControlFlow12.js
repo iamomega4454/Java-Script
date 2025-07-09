ec9b92a5fabff74529027feabee675f4c9d45f74
AMAL KURIAN ROYCE <145897989+iamomega4454@users.noreply.github.com>
2025-07-09 07:54:23 +0000

    ControlFlow12.js

--- basis/ControlFlow12.js
+++ basis/ControlFlow12.js
@@ -57,6 +57,20 @@ if (userloggin && debitcard) {
     }
 }
 
+// Nulish coalescing operator (??):null undefined ***************************
+let val1;
+//  val1=5 ?? 10    //5
+// val1=null ?? 15     // 15        used to check in database usually insted of 15 we use a complex function*****************************
+ val1=undefined ?? 20    //20
+console.log(val1);  
+
+//ternary operator(?)************************************
+const iceTeaPrice = 100
+iceTeaPrice >=80 ? console.log("more than 80") : console.log("less than 80");
+//more than 80
+
+
+
 // switch case
 const month=3
 switch (month) {
@@ -80,12 +94,44 @@ switch (month) {
         break;
 
     default:
-        console.log('invalid month');
+        console.log('invalid month');       //march
         break;
 }
 
 
+// truthy or falsey values
+const userEmail="a@mal.ai"      // if userEmail=[]  ans:got email
+
+if (userEmail) {                // if userEmail=""  ans:invalid email
+    console.log("got email");
+    
+} else {
+    console.log("invalid email");
+}
 
 
+// falsy values*********************************************************
+// false,0,-0,BigInt 0n,"",null,undefined,NaN
 
+//truthy values   any value inside a string**************************************
+// "0",'false'," ",[],{},function(){}
 
+// to check arrays
+const arr=[]    
+
+if (arr.length===0) {               
+    console.log("array is empty");
+    
+} else {
+    console.log("array is not empty");
+}       //array is empty
+
+// check for object
+const obj={}        // empty object
+
+if (Object.keys(obj).length===0) {   //*********************************************** */            
+    console.log("object is empty");
+    
+} else {
+    console.log("object is not empty");
+}  //object is empty
