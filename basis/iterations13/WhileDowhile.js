270ef39ea7a4aa9b5196a71a284613d3001e4de3
AMAL KURIAN ROYCE <145897989+iamomega4454@users.noreply.github.com>
2025-07-09 10:31:03 +0000

    WhileDowhile.js

---
+++ basis/iterations13/WhileDowhile.js
@@ -0,0 +1,31 @@
+let index=0;
+while (index<=10) {
+    console.log(`value is ${index}`);
+    index+=2
+}
+
+//array
+let myArr=["flash","Batman","Superman","Wondewomen"]
+let arr=0
+while (arr<myArr.length) {
+    console.log(`hero is ${myArr[arr]}`); //*********************************** */
+    arr++
+}
+// hero is flash
+// hero is Batman
+// hero is Superman
+// hero is Wondewomen
+
+
+//do while loop
+
+let score=1;
+do {
+    console.log(`score is ${score}`);
+    score++
+} while (score<=5);
+// score is 1
+// score is 2
+// score is 3
+// score is 4
+// score is 5
