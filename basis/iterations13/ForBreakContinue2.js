201b218a916ac6f22ca87fdfaf4a62e2f419cb9d
AMAL KURIAN ROYCE <145897989+iamomega4454@users.noreply.github.com>
2025-07-12 10:58:33 +0000

    ForBreakContinue2.js

--- basis/iterations13/ForBreakContinue2.js
+++ basis/iterations13/ForBreakContinue2.js
@@ -65,3 +65,57 @@ for (let i = 1; i <=10; i++) {
 // value of i is 9
 // value of i is 10
 
+
+//forof
+const arr = [1,2,3,4,5]
+
+for (const num of arr) {    // simple way of printing arrays******************
+    console.log(num);
+}
+
+const greetings = "Hello world"         // for string***************************
+    if(greetings.includes(" ")){
+        for (const greet of greetings) {
+            console.log(`Each char is ${greet}`);
+        }
+    }
+    else{
+        console.log("enter a valid greeting");
+    }
+
+
+//maps**********************************
+// it is an object which removes duplicated values and printe in order of input given
+const map=new Map();
+map.set('ind',"INDIA")
+map.set('fr',"FRANCE")
+map.set('USA',"AMMERICA")
+map.set('Ind',"INDIA")    // will not print
+
+console.log(map);
+// Map(4) {
+//   'ind' => 'INDIA',
+//   'fr' => 'FRANCE',
+//   'USA' => 'AMMERICA',
+//   'Ind' => 'INDIA'
+// }
+
+// using for loop 
+
+for (const key of map) {
+    console.log(key);        
+}
+// [ 'ind', 'INDIA' ]
+// [ 'fr', 'FRANCE' ]
+// [ 'USA', 'AMMERICA' ]
+// [ 'Ind', 'INDIA' ]
+
+for (const [key,value] of map) {//******************************************************* */
+    console.log(key,'-:',value);        // to remove [     ]
+}
+// ind -: INDIA
+// fr -: FRANCE
+// USA -: AMMERICA
+// Ind -: INDIA
+
+const my
